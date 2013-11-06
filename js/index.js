5291c2f46cb1137ba79d5feeab23cca68ccc81f5
marimari <mari@colinaroja.org>
2013-11-06 13:16:35 +0100

    pasar parámetro por referencia no por valor

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 $(function() {
 			
 	// setInterval's
-	var bucleActsky=0,
-		bucleActdona=0;
+	var bucles= new Array();
 
 	function skyactualiza(url, selector, empieza) {
 		var ahoraAct = new Date();
@@ -22,37 +21,21 @@ $(function() {
 	};
 
 	// definir cada bucle
-	function automatica(propioboton, idurl, idimg, idempieza, cadacuanto, idmensajeautomatico, idmensaje) {
+	function automatica(propioboton, idurl, idimg, idempieza, cadacuanto, idmensajeautomatico, idmensaje, posicionarray) {
 		var estado = $(propioboton).attr('data-estado');
 		if ( estado == 'apagado' ) {
 			$(propioboton).attr('disabled', 'disabled');
 			skyactualiza(idurl, idimg, idempieza);
-			
-			cualbu = $(propioboton).attr('data-inter');
-			
-			if ( cualbu == 'sky' ) {
-				bucleActsky = setInterval( function() {
+						
+			bucles[posicionarray] = setInterval( function() {
 					skyactualiza(idurl, idimg, idempieza);
-				}, cadacuanto);
-			}
-			else {
-				bucleActdona = setInterval( function() {
-					skyactualiza(idurl, idimg, idempieza);
-				}, cadacuanto);
-			}
-
+			}, cadacuanto);
+			
 			$(propioboton).attr({ 'data-estado': 'encendido', class: 'btn btn-success btn-sm' }).removeAttr('disabled').html(idmensajeautomatico);
 		}
 		else {
 			$(propioboton).attr({ 'data-estado': 'apagado', class: 'btn btn-sm' }).html(idmensaje);
-			cualbu = $(propioboton).attr('data-inter');
-			if ( cualbu == 'sky' ) {
-				clearInterval(bucleActsky);
-			}
-			else {
-				clearInterval(bucleActdona);
-			}
-			
+			clearInterval(bucles[posicionarray]);
 		}
 	};
 	
@@ -64,16 +47,17 @@ $(function() {
 	
 	// Actualiza cada 5 minutos
 	$('#skymatic').on('click', function() {
-		automatica($(this), 'http://150.214.162.13/skycamera/goto/SONA%20CIECEM/img?', '#skyimg', 'img/empieza.jpg', 300000, 'Apagar automático <span class="glyphicon glyphicon-off"></span>', 'Actualizar cada 5 minutos <span class="glyphicon glyphicon-refresh"></span>');
+		automatica($(this), 'http://150.214.162.13/skycamera/goto/SONA%20CIECEM/img?', '#skyimg', 'img/empieza.jpg', 300000, 'Apagar automático <span class="glyphicon glyphicon-off"></span>', 'Actualizar cada 5 minutos <span class="glyphicon glyphicon-refresh"></span>', 0);
 	});
 	
 	// Actualiza cada 2 segundos
 	$('#donanamatic').on('click', function() {
-		automatica($(this), 'http://150.214.162.14:8081/snapshot.cgi?user=visitor&pwd=&', '#camC', 'img/empieza1.jpg', 2000, 'Apagar automático <span class="glyphicon glyphicon-off"></span>', 'Actualizar cada 2 segundos <span class="glyphicon glyphicon-refresh"></span>');
+		automatica($(this), 'http://150.214.162.14:8081/snapshot.cgi?user=visitor&pwd=&', '#camC', 'img/empieza1.jpg', 2000, 'Apagar automático <span class="glyphicon glyphicon-off"></span>', 'Actualizar cada 2 segundos <span class="glyphicon glyphicon-refresh"></span>', 1);
 	});
 
 	//si pulsa algo del menú
-	$('.cambio').on('click', function() {
+	$('.cambio').on('click', function(e) {
+		e.preventDefault();
 		$('.cambio').parent().removeClass('active');
 		$(this).parent().addClass('active');
 		$('.diapositiva').hide();
@@ -81,7 +65,8 @@ $(function() {
 		$(cualmuestro).show().removeClass('invisible');
 	});
 	
-	$('.cambioboton').on('click', function() {
+	$('.cambioboton').on('click', function(e) {
+		e.preventDefault();
 		$('.cambioboton').removeAttr('disabled');
 		$(this).attr('disabled', 'disabled');
 		$('.diapositiva').hide();
@@ -90,8 +75,8 @@ $(function() {
 	});
 
 	// empieza
-	skyactualiza('http://150.214.162.13/skycamera/goto/SONA%20CIECEM/img?', '#skyimg', 'img/empieza.jpg', 0);
-	skyactualiza('http://150.214.162.14:8081/snapshot.cgi?user=visitor&pwd=&', '#camC', 'img/empieza1.jpg', 1);
+	skyactualiza('http://150.214.162.13/skycamera/goto/SONA%20CIECEM/img?', '#skyimg', 'img/empieza.jpg');
+	skyactualiza('http://150.214.162.14:8081/snapshot.cgi?user=visitor&pwd=&', '#camC', 'img/empieza1.jpg');
 
 	$('#slidedonana, #slideskye').tooltip({container: 'body'});
 			
